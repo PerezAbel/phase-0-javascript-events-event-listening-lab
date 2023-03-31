eba17e4d773af6bc331a53bc67b7e94b38353459
PerezAbel <126673313+PerezAbel@users.noreply.github.com>
2023-03-31 10:23:48 +0300

    "Completed assignment"

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
-}
+// Select the button
+const button = document.getElementById('button');
+
+// Attach an event listener to the button
+button.addEventListener('click', function() {
+  // Define the action(s) to take when the button is clicked
+  console.log('Button clicked!');
+});
